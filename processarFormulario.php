665310540b17d2e639c9d78f5a264fe9a6d633a9
Layrane Leão <contato.layrane@gmail.com>
2025-09-14 08:59:00 -0300

    chore: criar arquivo base de validação de formulário em PHP

--- processarFormulario.php
+++ processarFormulario.php
@@ -1,3 +1,12 @@
 ﻿<?php
-
+    $nome = $_POST['nome'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $telefone = $_POST['telefone'] ?? null;
+    $cargo = $_POST['cargo'] ?? null;
+    $empresa = $_POST['empresa'] ?? null;
+    $site = $_POST['site'] ?? null;
+    $investmentoEmMarketing = $_POST['marketing'] ?? null;
+    // Falta o campo de serviços
+    $maisInformacoes = $_POST['mais-info'] ?? null;
+    $checkboxNewsletter = $_POST['check-newsletter'] ?? null;
 ?>
